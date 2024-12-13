88a624307456f49843b49823abb3698ad26b7b72
Fl4m3l1ng <rademaat@miamioh.edu>
2024-12-12 22:38:08 -0500

    Camera and Level Update
    Features:
    - Camera now moves around with the player into each room
    - Level graphics are tweaked to better fit with the area
    
    Edits:
    - Rearranged room prefabs to be more organized
    
    Fixes:
    - Fixed stairs moving you up to high

--- IMS465Game/Assets/Scripts/Objects/Item.cs
+++ IMS465Game/Assets/Scripts/Objects/Item.cs
@@ -48,7 +48,7 @@ public class Item : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("We colidied with: " + collision.gameObject.name);
+        //Debug.Log("We colidied with: " + collision.gameObject.name);
 
         // If colliding with the player
         if (collision.gameObject.CompareTag("Player"))
